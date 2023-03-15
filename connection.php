6aed00638b0f2f31e3f086ded315b06639306315
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-03-15 19:58:59 +0530

    create connection.php file for connect data base

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$user = 'root';
+
+$password = '';
+
+$db = 'cart';
+
+$host = 'localhost';
+
+$port = 3306;
+
+$conn = mysqli_connect($host, $user, $password, $db, $port);
+
+if (!$conn) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
